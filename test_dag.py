c8398f9b61f1a91542f387e4c29915d721682d87
ashwin-menon-hs <ashwin.menon@hootsuite.com>
2025-11-14 18:21:11 -0500

    initial commit

---
+++ test_dag.py
@@ -0,0 +1,22 @@
+from airflow.decorators import dag, task
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def print_test_message(**kwargs):
+    conf = kwargs['dag_run'].conf
+    message = conf.get("message", "this is a DAG used for Airflow validation")
+    print(f"message from user => {message}")
+
+@dag(
+    start_date=datetime(2025, 7, 1),
+    schedule=None,
+    catchup=False,
+    tags=['test_operation', 'slack_test'],
+)
+def test_dag():
+    test_dag_operation = PythonOperator(
+        task_id='test_dag_operation',
+        python_callable=print_test_message
+    )
+    test_dag_operation
+test_dag()
